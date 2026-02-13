9c8f339675e68861a7570b8d001ff840cf91561c
AlexReoyo <reoyonavasalex30@gmail.com>
2026-02-13 16:38:40 +0100

    Se añade método para cambiar la dirección de la nave según el clic en la pantalla, también se ñade la variable velocidad general para las naves y un objeto naveAmiga dentro del controlador

--- SpaceInvaders-Android/core/src/main/java/com/politecnicomalaga/sp/control/Controlador.java
+++ SpaceInvaders-Android/core/src/main/java/com/politecnicomalaga/sp/control/Controlador.java
@@ -1,15 +1,25 @@
 package com.politecnicomalaga.sp.control;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.politecnicomalaga.sp.model.NaveAmi;
+import com.politecnicomalaga.sp.model.Ovni;
 
 import java.util.Map;
 
 public class Controlador {
     private static Controlador miSingle;
+    private NaveAmi naveAmiga;
+    public float velocidadNave;
+
+    //CONSTRUCTOR
     private Controlador() {
+        naveAmiga = new NaveAmi();
+        velocidadNave = 1.5f;
     }
 
+    //Otros métodos
     public static Controlador getInstance(){
         if (miSingle == null){
             miSingle= new Controlador();
@@ -17,22 +27,26 @@ public class Controlador {
             return miSingle;
     }
     public void click (float x, float y){
-
+        cambiarSentidoNaveAmiga(x);
     }
     public void simulaMundo(){
-        /*
-        he muerto
-        cada enemigo: ¿he muerto?
-        ¿toca disparar? (jugador)
-        cada enemigo: ¿toca disparar?
-        ¿hay que mover la nave jugador?
-         */
-
-
+        naveAmiga.mover(naveAmiga.getDir(),velocidadNave);
     }
 
     public void pintar(SpriteBatch batch, Map<String, Texture> galeriaImagenes){
 
+    }
+
+    public void cambiarSentidoNaveAmiga (float x){
+        if (x>naveAmiga.getX() && naveAmiga.getDir()!= Ovni.Direccion.DERECHA){
+            naveAmiga.setDir(Ovni.Direccion.DERECHA);
+        } else if (x>naveAmiga.getX() && naveAmiga.getDir()== Ovni.Direccion.DERECHA) {
+           naveAmiga.setDir(Ovni.Direccion.NOMOVER);
+        } else if (x<naveAmiga.getX() && naveAmiga.getDir()!= Ovni.Direccion.IZQUIERDA){
+            naveAmiga.setDir(Ovni.Direccion.IZQUIERDA);
+        } else if (x<naveAmiga.getX() && naveAmiga.getDir() == Ovni.Direccion.IZQUIERDA){
+            naveAmiga.setDir(Ovni.Direccion.NOMOVER);
+        }
     }
 
 
