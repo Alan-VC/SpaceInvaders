904f8684ff66fd58f522b71cb485c5d345e2e98f
AndresGorea <andres.g.principal@gmail.com>
2026-02-25 16:26:00 +0100

    desacoplar DisparoEne de la vista y corregir lógica de estados

--- SpaceInvaders-Android/core/src/main/java/com/politecnicomalaga/sp/model/DisparoEne.java
+++ SpaceInvaders-Android/core/src/main/java/com/politecnicomalaga/sp/model/DisparoEne.java
@@ -1,22 +1,22 @@
 package com.politecnicomalaga.sp.model;
 
-import com.badlogic.gdx.Gdx;
-
 public class DisparoEne extends Disparo{
     public DisparoEne(float x, float y, float width, float height, Estado estado, Direccion dir, String textura) {
         super(x, y, width, height, estado, dir, textura);
 
     }
     @Override
-    public boolean desaparecer() {
-        if (this.getY()<=0) {
+    public void desaparecer(float limiteInferior) {
+        if (this.getY()<=limiteInferior) {
             this.setEstado(Estado.MUERTO);
-            return true;
         }
-        return false;
     }
 
     public boolean comprobarColision(NaveAmi naveAmi) {
+        // Si ya impactó (y está pendiente de borrar) evitamos "doblekill"
+        // Y evitamos que una bala muerta siga matando mientras espera ser borrada
+        if (this.getEstado() == Estado.MUERTO) return false;
+
         if (naveAmi.estaVivo() && this.colision(naveAmi)) {
             naveAmi.recibirDisparo();
             this.setEstado(Estado.MUERTO);
