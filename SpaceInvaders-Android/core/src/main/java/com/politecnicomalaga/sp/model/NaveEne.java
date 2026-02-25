75b087d112790ba4d16db0bb0d8e35e912acb857
AndresGorea <andres.g.principal@gmail.com>
2026-02-25 13:19:59 +0100

    adaptar a herencia de Nave y eliminar atributos duplicados

--- SpaceInvaders-Android/core/src/main/java/com/politecnicomalaga/sp/model/NaveEne.java
+++ SpaceInvaders-Android/core/src/main/java/com/politecnicomalaga/sp/model/NaveEne.java
@@ -6,20 +6,17 @@ public class NaveEne extends Nave{
 
     private int probabilidadDisparo; //Entre 0 y 100 la seteamos en el controlador
     private ArrayList<DisparoEne> misDisparos; //Lista de disparos
-    private float anchoBala;
-    private float altoBala;
 
-    public NaveEne(float x, float y, float width, float height, Estado estado, Direccion dir, String textura, int vidas, float cadencia, int probabilidadDisparo, float anchoBala, float altoBala) {
-        super(x, y, width, height, estado, dir, textura, vidas, cadencia);
+    public NaveEne(float x, float y, float width, float height, Estado estado, Direccion dir, String textura, int vidas, float cadencia, float anchoBala, float altoBala, float velocidadBala, int probabilidadDisparo) {
+        super(x, y, width, height, estado, dir, textura, vidas, cadencia, anchoBala, altoBala, velocidadBala);
         this.probabilidadDisparo = probabilidadDisparo;
         this.misDisparos = new ArrayList<>();
-        this.anchoBala = anchoBala;
-        this.altoBala = altoBala;
     }
 
     public int getProbabilidadDisparo() {
         return probabilidadDisparo;
     }
+
     public void setProbabilidadDisparo(int probabilidadDisparo) {
         this.probabilidadDisparo = probabilidadDisparo;
     }
@@ -38,11 +35,11 @@ public class NaveEne extends Nave{
         if (estaVivo()) {
             if (Math.random() * 100 < getProbabilidadDisparo()) {
                 // Cálculo dinámico para centrar la bala perfectamente
-                float posX = (getX() + getMitadWidth()) - (this.anchoBala / 2f);
-                float posY = getY() - this.altoBala;
+                float posX = (getX() + getMitadWidth()) - (getAnchoBala() / 2f);
+                float posY = getY() - getAltoBala();
 
                 // Creamos el disparo y lo añadimos a la lista
-                DisparoEne nuevoDisparo = new DisparoEne(posX, posY, this.anchoBala, this.altoBala, Estado.VIVO, Direccion.ABAJO, "disparoEne.png");
+                DisparoEne nuevoDisparo = new DisparoEne(posX, posY, getAnchoBala(), getAltoBala(), Estado.VIVO, Direccion.ABAJO, "disparoEne.png");
                 misDisparos.add(nuevoDisparo);
             }
         }
@@ -54,7 +51,7 @@ public class NaveEne extends Nave{
 
             //Si el disparo esta vivo es decir no ha colisionado lo movemos
             if (d.getEstado() == Estado.VIVO){
-                d.mover(Direccion.ABAJO, 5f);
+                d.mover(Direccion.ABAJO, getVelocidadBala());
                 d.desaparecer(); //Preguntamos si se ha salido de la pantalla y este setea el estado a muerto
             }
             if (d.getEstado() == Estado.MUERTO) misDisparos.remove(i); //Lo eliminamos si la comprobación de desaparecer de la pantalla ya nos da que esta Muerto
