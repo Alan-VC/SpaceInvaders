2dc99a4d617038dedf59e2b0e98b1e4a5b16ebc6
AlexReoyo <reoyonavasalex30@gmail.com>
2026-02-26 15:01:56 +0100

    Se cambia el método pintar para poder pintar todos los elementos de la pantalla y no solo de la nave amiga

--- SpaceInvaders-Android/core/src/main/java/com/politecnicomalaga/sp/control/Controlador.java
+++ SpaceInvaders-Android/core/src/main/java/com/politecnicomalaga/sp/control/Controlador.java
@@ -3,19 +3,27 @@ package com.politecnicomalaga.sp.control;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.politecnicomalaga.sp.model.Batallon;
+import com.politecnicomalaga.sp.model.DisparoAmi;
+import com.politecnicomalaga.sp.model.DisparoEne;
+import com.politecnicomalaga.sp.model.Escuadron;
 import com.politecnicomalaga.sp.model.NaveAmi;
+import com.politecnicomalaga.sp.model.NaveEne;
 import com.politecnicomalaga.sp.model.Ovni;
 
+import java.util.List;
 import java.util.Map;
 
 public class Controlador {
     private static Controlador miSingle;
     private NaveAmi naveAmiga;
+    private Batallon batallon;
     public float velocidadNave;
 
     //CONSTRUCTOR
     private Controlador() {
         naveAmiga = new NaveAmi(300,0,80,70, Ovni.Estado.VIVO, Ovni.Direccion.NOMOVER,"naveJugador.png",1,4,20,45,10);
+        batallon = new Batallon();
         velocidadNave = 1.5f;
     }
 
@@ -38,14 +46,34 @@ public class Controlador {
             naveAmiga.setX(0);
             naveAmiga.setDir(Ovni.Direccion.NOMOVER);
         }
+
         naveAmiga.mover(naveAmiga.getDir(),velocidadNave);
     }
 
     public void pintar(SpriteBatch batch, Map<String, Texture> galeriaImagenes){
+        //pintar naveAmiga
         batch.draw(galeriaImagenes.get(naveAmiga.getTextura()),naveAmiga.getX(),naveAmiga.getY(),naveAmiga.getWidth(),naveAmiga.getHeight());
 
-        //en un futuro se implementa el pintar de batallón cuando esté programado.
-        //batallon.pintar(batch);
+        //pintar navesEnemigas y sus disparos
+        Escuadron[] escuadrones = batallon.getEscuadrones();
+        for (Escuadron esc: escuadrones){
+            NaveEne[] naveEnes = esc.getNavesEnemigas();
+            for (NaveEne navE: naveEnes){
+                if (navE.estaVivo()) {
+                    batch.draw(galeriaImagenes.get(navE.getTextura()), navE.getX(), navE.getY(), navE.getWidth(), navE.getHeight());
+                    List<DisparoEne> disparosEnemigos = navE.getMisDisparos();
+                    for (DisparoEne disEne: disparosEnemigos){
+                        batch.draw(galeriaImagenes.get(disEne.getTextura()),disEne.getX(),disEne.getY(),disEne.getWidth(),disEne.getHeight());
+                    }
+                }
+            }
+        }
+        //Pintar disparosAmigos
+        List<DisparoAmi> disparosAmigos = naveAmiga.getMisDisparos();
+        for (DisparoAmi dispAmi: disparosAmigos){
+            batch.draw(galeriaImagenes.get(dispAmi.getTextura()),dispAmi.getX(),dispAmi.getY(),dispAmi.getWidth(),dispAmi.getHeight());
+        }
+
     }
 
     public void cambiarSentidoNaveAmiga (float x){
